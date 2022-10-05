e6abedc1d8a83365d8aa77566c9eab9f4a954755
DoolyBoi <abdullah.havaldar@oit.edu>
2022-10-05 16:12:18 -0700

    made documentation

---
+++ CST116-Proj1-Part1-Havaldar.cpp
@@ -0,0 +1,15 @@
+/*
+CST116 C++ Programming
+Project 1 Part 1
+Abdullah Havaldar
+	Phone: 503-764-5753
+	Email: abdullah.havaldar@oit.edu
+
+Purpose:
+To take in the Name, Income, and GPA of 4 people
+Print them out with appropriate headings
+Headings and data must line up
+Must use Manipulators
+Use appropriate data types to store the data
+
+*/
